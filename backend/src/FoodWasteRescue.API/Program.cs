a47c631eb6c81191b14457d489fa02ced6bbd70c
TRIDENT\theoh <theohar@gmail.com>
2026-05-11 18:29:18 +0300

    Fixed web.config

--- backend/src/FoodWasteRescue.API/Program.cs
+++ backend/src/FoodWasteRescue.API/Program.cs
@@ -34,11 +34,8 @@ var app = builder.Build();
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseCors("ReactFrontend");
 
